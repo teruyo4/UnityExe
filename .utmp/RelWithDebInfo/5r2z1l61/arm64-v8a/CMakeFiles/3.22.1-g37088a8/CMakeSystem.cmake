def2c48b5a4b104c0e3ab95fe9497b8772519a31
teruyo4 <teruyo4@gmail.com>
2025-10-03 00:19:17 +0900

    test oppo reno a

---
+++ .utmp/RelWithDebInfo/5r2z1l61/arm64-v8a/CMakeFiles/3.22.1-g37088a8/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-6.14.0-32-generic")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "6.14.0-32-generic")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/home/teruyo4/Unity/Hub/Editor/6000.2.6f1/Editor/Data/PlaybackEngines/AndroidPlayer/NDK/build/cmake/android.toolchain.cmake")
+
+set(CMAKE_SYSTEM "Android-1")
+set(CMAKE_SYSTEM_NAME "Android")
+set(CMAKE_SYSTEM_VERSION "1")
+set(CMAKE_SYSTEM_PROCESSOR "aarch64")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
--- Assets/script/UIController.cs
+++ Assets/script/UIController.cs
@@ -8,6 +8,7 @@ public class UIController : MonoBehaviour
 {
     [SerializeField] GameObject sampleElement;
     private UIDocument _uiDocument;
+    private Label numLabel;
     
     void Start() {
 	var bo = Instantiate(sampleElement);
@@ -16,8 +17,16 @@ public class UIController : MonoBehaviour
 	// set click function
 	foreach (int i in new List<int> {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}) {
 	    var btn = _uiDocument.rootVisualElement.Q<Button>($"button{i}");
-	    btn.clickable.clicked += () => {Debug.Log($"clicked{i}");};
+	    btn.clickable.clicked += () => {InputNumberLine(i);};
 	}
-	
+	numLabel = _uiDocument.rootVisualElement.Q<Label>("NumLabelStr");
+	numLabel.text = "";
+    }
+
+    void InputNumberLine(int num) {
+	string str = (string)numLabel.text;
+	if (str.Length > 7)
+	    str = str.Substring(1, 7);
+	numLabel.text = $"{str}{num}";
     }
 }
